824ed4d29aad2b41419b105f1856d1fab150e940
Kamil Grześkowiak <kgrzeskowiak3@edu.cdv.pl>
2024-06-01 15:57:29 +0200

    Added attack script and started work on enemy

---
+++ FirstGame/Assets/Scripts/Attack.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public Animator animator;
+    public float attackRange = 1f;
+    public int attackDamage = 10;
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            MeleeAttack();
+        }
+    }
+
+    void MeleeAttack()
+    {
+        animator.SetTrigger("Attack");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    // Metoda rysuj¹ca zasiêg ataku w edytorze Unity
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
