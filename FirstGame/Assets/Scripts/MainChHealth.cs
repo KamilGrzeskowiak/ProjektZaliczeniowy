50f98ee393a660a6a54dc8e24d7d801c903b1fdf
Kamil Grześkowiak <kgrzeskowiak3@edu.cdv.pl>
2024-07-04 10:22:37 +0200

    Game finished!

--- FirstGame/Assets/Scripts/MainChHealth.cs
+++ FirstGame/Assets/Scripts/MainChHealth.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public int maxHealth = 100;
+    public string sceneToLoad;
     private int currentHealth;
     private Animator animator;
 
@@ -37,15 +39,6 @@ public class Player : MonoBehaviour
     void Die()
     {
         Debug.Log("Player died!");
-        if (animator != null)
-        {
-            animator.SetTrigger("Death");
-        }
-        else
-        {
-            Debug.LogError("Animator is null in Die method");
-        }
-        // Opcjonalnie, mo¿esz dodaæ opóŸnienie przed zniszczeniem obiektu, aby umo¿liwiæ odtworzenie animacji œmierci
-        // Destroy(gameObject, 1.5f); // Zniszcz obiekt po 1.5 sekundzie (dopasuj do d³ugoœci animacji œmierci)
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
